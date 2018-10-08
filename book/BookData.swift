43d4d8f6c94e95f0d1839f84f20ea229dd2cfb44
D7703_08 <d7703_08@d7703-08ui-imac.local>
2018-10-08 12:30:37 +0900

    123

---
+++ book/BookData.swift
@@ -0,0 +1,13 @@
+//
+//  BookData.swift
+//  book
+//
+//  Created by D7703_08 on 2018. 10. 2..
+//  Copyright © 2018년 D7703_08. All rights reserved.
+//
+
+import Foundation
+class BookData {
+    var title = ""
+    var author = ""
+}
